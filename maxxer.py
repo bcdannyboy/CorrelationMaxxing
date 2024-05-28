6d383a36acb6375ebf9a25ad6b550b71f785ed5f
Daniel Bloom <daniel@redteamarts.com>
2024-05-28 16:44:16 -0700

    update rf

--- maxxer.py
+++ maxxer.py
@@ -233,15 +233,15 @@ def optimize_portfolio_rf(stock_data, commodity_correlations, cboe_correlations,
     # Train Random Forest Regressor with extensive parameters
     rf = RandomForestRegressor(
         n_estimators=10000,
-        max_depth=None,
+        max_depth=50,
         min_samples_split=2,
         min_samples_leaf=1,
-        max_features='auto',
+        max_features='sqrt',
         n_jobs=-1,
         random_state=42,
         bootstrap=True,
         oob_score=True,
-        verbose=1
+        verbose=2  # Set verbose to 2 for more detailed output
     )
     rf.fit(X_train, y_train)
     
